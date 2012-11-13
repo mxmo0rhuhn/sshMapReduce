7a8efc0244b4fb43f63a16c74bc423c73957da7a
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-13 11:35:41 +0100

    added PoolTest pop() and getFreeWorkers()

--- src/test/java/ch/zhaw/dna/ssh/mapreduce/PoolTest.java
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/PoolTest.java
@@ -22,4 +22,51 @@ public class PoolTest {
 	public void invalidNumerOfPoolWorkers() {
 		new Pool(0);
 	}
+	
+	@Test
+	public void testPopPool5Workers() {
+		Pool pool = new Pool(5);
+		pool.pop();
+		assertEquals("Gibt Capacity - pop() = AnzVerfuegbare Worker", 4, pool.getFreeWorkers());
+	}
+	
+	@Test
+	public void testPopPool1Workers() {
+		Pool pool = new Pool(1);
+		pool.pop();
+		assertEquals("Gibt Capacity - pop() = AnzVerfuegbare Worker", 0, pool.getFreeWorkers());
+	}
+	
+	@Test
+	public void testPopPoolWorkers() {
+		Pool pool = new Pool(1);
+		assertNotNull(pool.pop());
+		assertNull(pool.pop());
+	}
+	
+	@Test
+	public void testPopPool2Workers() {
+		Pool pool = new Pool(2);
+		assertNotNull(pool.pop());
+		assertNotNull(pool.pop());
+		assertNull(pool.pop());
+	}
+	
+	@Test
+	public void testPopPool3Workers() {
+		Pool pool = new Pool(3);
+		assertNotNull(pool.pop());
+		assertNotNull(pool.pop());
+		assertNotNull(pool.pop());
+		assertNull(pool.pop());
+	}
+	
+	@Test
+	public void testPopPoolLoopWorkers() {
+		Pool pool = new Pool(1000);
+		for (int i=0; i < 1000; i++) {
+			assertNotNull(pool.pop());
+		}
+		assertNull(pool.pop());
+	}
 }
