bfc34b7327ec42ebf13009a2d7d4dfb605256366
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-13 12:17:34 +0100

    added Worker/Factory

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/Pool.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/Pool.java
@@ -2,12 +2,14 @@ package ch.zhaw.dna.ssh.mapreduce;
 
 /**
  * Pool kontrolliert Anzahl der Worker und liefert diese an Master
+ * 
  * @author Desiree Sacher
- *
+ * 
  */
 public class Pool {
 
-	private final int capacity;
+	private final Worker[] workerList;
+	private int pos = 0;
 
 	/**
 	 * Setzt Pool Groesse = Anz Worker durch Uebergabewert fest
@@ -16,20 +18,49 @@ public class Pool {
 	 *             Wenn Wert kleiner 1
 	 * @param capacity
 	 *            Groesse von Pool
+	 * @param factory
+	 *            Neue Worker werden durch Factory Pattern erstellt
 	 */
-	public Pool(int capacity) {
+	public Pool(int capacity, WorkerFactory factory) {
 		if (capacity < 1) {
 			throw new IllegalArgumentException("Falsche Eingabe, muss groesser 1 sein");
 		}
-		this.capacity = capacity;
+		workerList = new Worker[capacity];
+		for (int i = 0; i < workerList.length; i++) {
+			workerList[i] = factory.createWorker();
+		}
 	}
 
 	/**
-	 * Gibt Totale Anzahl Worker zurück
+	 * Gibt Totale Anzahl Worker zurueck
+	 * 
 	 * @return amountWorker
 	 */
 	public int getCapacity() {
-		return capacity;
+		return workerList.length;
+	}
+
+	/**
+	 * Gibt Anzahl Verfuegbare Workers zurueck
+	 * @return Anz Verfuegbare Workers
+	 */
+	public int getFreeWorkers() {
+		return workerList.length - pos;
+	}
+
+	/**
+	 * Weist Worker zu und entfernt diesen aus verfuegbare Workers
+	 * @return Worker wenn noch verfuegbar, sonst null
+	 */
+	public Worker pop() {
+		if (getFreeWorkers() < 1) {
+			return null;
+		} else {
+			Worker w = workerList[pos];
+			workerList[pos] = null;
+			pos++;
+			return w;
+		}
 	}
 
 }
--- src/test/java/ch/zhaw/dna/ssh/mapreduce/PoolTest.java
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/PoolTest.java
@@ -2,51 +2,53 @@ package ch.zhaw.dna.ssh.mapreduce;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PoolTest {
 
-	/**
-	 * Pool verwaltet Ressourcen und vergibt Auftraege an worker
-	 * 
-	 * Konstruktor: erstellt Pool(AnzWorker)
-	 */
+	private static WorkerFactory factoryMock;
+	
+	@BeforeClass
+	public static void initMock() {
+		factoryMock = new MockFactory();
+	}
 	
 	@Test
 	public void testNumerOfPoolWorkers() {
-		Pool pool = new Pool(5);
+		Pool pool = new Pool(5, factoryMock);
 		assertEquals("Pruefen ob korrekte Anz Worker im Pool erstellt wurden: ", 5, pool.getCapacity());
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void invalidNumerOfPoolWorkers() {
-		new Pool(0);
+		new Pool(0, factoryMock);
 	}
 	
 	@Test
 	public void testPopPool5Workers() {
-		Pool pool = new Pool(5);
+		Pool pool = new Pool(5, factoryMock);
 		pool.pop();
 		assertEquals("Gibt Capacity - pop() = AnzVerfuegbare Worker", 4, pool.getFreeWorkers());
 	}
 	
 	@Test
 	public void testPopPool1Workers() {
-		Pool pool = new Pool(1);
+		Pool pool = new Pool(1, factoryMock);
 		pool.pop();
 		assertEquals("Gibt Capacity - pop() = AnzVerfuegbare Worker", 0, pool.getFreeWorkers());
 	}
 	
 	@Test
 	public void testPopPoolWorkers() {
-		Pool pool = new Pool(1);
+		Pool pool = new Pool(1, factoryMock);
 		assertNotNull(pool.pop());
 		assertNull(pool.pop());
 	}
 	
 	@Test
 	public void testPopPool2Workers() {
-		Pool pool = new Pool(2);
+		Pool pool = new Pool(2, factoryMock);
 		assertNotNull(pool.pop());
 		assertNotNull(pool.pop());
 		assertNull(pool.pop());
@@ -54,7 +56,7 @@ public class PoolTest {
 	
 	@Test
 	public void testPopPool3Workers() {
-		Pool pool = new Pool(3);
+		Pool pool = new Pool(3, factoryMock);
 		assertNotNull(pool.pop());
 		assertNotNull(pool.pop());
 		assertNotNull(pool.pop());
@@ -63,10 +65,22 @@ public class PoolTest {
 	
 	@Test
 	public void testPopPoolLoopWorkers() {
-		Pool pool = new Pool(1000);
+		Pool pool = new Pool(1000, factoryMock);
 		for (int i=0; i < 1000; i++) {
 			assertNotNull(pool.pop());
 		}
 		assertNull(pool.pop());
+	}
+	
+	private static class MockFactory implements WorkerFactory{
+
+		@Override
+		public Worker createWorker() {
+			
+			return new Worker(){
+				
+			};
+		}
+		
 	}
 }
