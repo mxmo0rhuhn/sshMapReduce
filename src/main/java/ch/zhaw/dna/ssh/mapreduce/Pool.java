14242eb786d850f27ab4ee60577a1a0d3cb9b4de
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-13 11:17:20 +0100

    PoolTest finished

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/Pool.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/Pool.java
@@ -1,17 +1,31 @@
 package ch.zhaw.dna.ssh.mapreduce;
 
+/**
+ * Pool kontrolliert Anzahl der Worker und liefert diese an Master
+ * @author Desiree Sacher
+ *
+ */
 public class Pool {
-	
+
 	private final int amountWorker;
 
+	/**
+	 * Setzt Pool Groesse = Anz Worker durch Uebergabewert fest
+	 * 
+	 * @throws IllegalArgumentException
+	 *             Wenn Wert kleiner 1
+	 * @param amountWorker
+	 *            Groesse von Pool
+	 */
 	public Pool(int amountWorker) {
+		if (amountWorker < 1) {
+			throw new IllegalArgumentException("Falsche Eingabe, muss groesser 1 sein");
+		}
 		this.amountWorker = amountWorker;
-		
 	}
 
-	public Object getPoolSize() {
-		// TODO Auto-generated method stub
-		return null;
+	public int getPoolSize() {
+		return amountWorker;
 	}
 
 }
