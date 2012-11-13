ea22f5617c4a5b70030cba8efca7e90be48a8ab3
des <des@foxy.(none)>
2012-11-13 10:59:51 +0100

    added 1st tests PoolTest

--- src/test/java/ch/zhaw/dna/ssh/mapreduce/PoolTest.java
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/PoolTest.java
@@ -1,8 +1,25 @@
 package ch.zhaw.dna.ssh.mapreduce;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class PoolTest {
 
 	/**
 	 * Pool verwaltet Ressourcen und vergibt Auftraege an worker
+	 * 
+	 * Konstruktor: erstellt Pool(AnzWorker)
 	 */
+	
+	@Test
+	public void testNumerOfPoolWorkers() {
+		Pool pool = new Pool(5);
+		assertEquals("Pruefen ob korrekte Anz Worker im Pool erstellt wurden: ", 5, pool.getPoolSize());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void invalidNumerOfPoolWorkers() {
+		new Pool(0);
+	}
 }
