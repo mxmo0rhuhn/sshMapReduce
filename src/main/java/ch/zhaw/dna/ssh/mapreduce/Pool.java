234d54b049e3312b6ea793c7abe8ec65b81f438a
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-13 11:03:04 +0100

    PoolTest

---
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/Pool.java
@@ -0,0 +1,17 @@
+package ch.zhaw.dna.ssh.mapreduce;
+
+public class Pool {
+	
+	private final int amountWorker;
+
+	public Pool(int amountWorker) {
+		this.amountWorker = amountWorker;
+		
+	}
+
+	public Object getPoolSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
