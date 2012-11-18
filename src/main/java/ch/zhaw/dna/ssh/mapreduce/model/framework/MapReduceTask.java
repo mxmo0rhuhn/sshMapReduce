0de414fb00959fa4ea770a7b5c5c7f9749fa335b
Reto Habluetzel <rethab@rethab.ch>
2012-11-18 18:25:51 +0100

    refactored mapreducetask

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/MapReduceTask.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/MapReduceTask.java
@@ -1,8 +1,11 @@
 package ch.zhaw.dna.ssh.mapreduce.model.framework;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import ch.zhaw.dna.ssh.mapreduce.model.framework.impl.PooledMapRunnerFactory;
 
 /**
  * Dies ist ein neuer MapReduce Task. Er ist für seine Worker der Master.
@@ -11,30 +14,33 @@ import java.util.Map;
  */
 public class MapReduceTask {
 
-	MapTask mapTask;
+	private final MapTask mapTask;
 
-	ReduceTask reduceTask;
+	private final ReduceTask reduceTask;
 
-	MapRunnerFactory myFactory;
+	private final MapRunnerFactory mapRunnerFactory;
 	
-	Pool p = new Pool(); // TODO Scope
-	
-	
-
 	public MapReduceTask(MapTask mapTask, ReduceTask reduceTask) {
 		this.mapTask = mapTask;
 		this.reduceTask = reduceTask;
-		myFactory = new PooledMapRunnerFactory();
-		myFactory.assignMapTask(mapTask);
-		
+		this.mapRunnerFactory = new PooledMapRunnerFactory();
+		this.mapRunnerFactory.assignMapTask(mapTask);
 	}
 
 	public Map<String, List<String>> compute(String input) {
-		MapRunner mapRunenr = myFactory.getMapRunner();
-		mapRunenr.runMapTask(input.split(" "), this.p);
+		InputSplitter inputSplitter = InputSplitterFactory.create(input);
+		List<MapRunner> mapRunners = new LinkedList<MapRunner>();
+		while (inputSplitter.hasNext()) {
+			MapRunner mapRunner = mapRunnerFactory.getMapRunner();
+			mapRunners.add(mapRunner);
+			mapRunner.runMapTask(inputSplitter.next().split(" "));
+		}
+		
+		
+		
 		Map<String, List<String>> results = new HashMap<String, List<String>>();
-		ReduceRunner reduceRunner = null;
-		reduceRunner.reduce(mapRunenr.getIntermediate());
+		ReduceRunner reduceRunner = this.reduceRunnerFactory.createReduceRunner();
+		reduceRunner.reduce(.getIntermediate());
 		return results;
 	}
 
