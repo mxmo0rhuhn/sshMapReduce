a8d6dc60693794b447258fd4a4be5b68af7afa4d
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-18 18:46:50 +0100

    adjusted design decision in WordFrequencyReduceTask2

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/WordFrequencyReduceTask.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/WordFrequencyReduceTask.java
@@ -1,21 +1,30 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
+import java.util.List;
+
+import ch.zhaw.dna.ssh.mapreduce.model.framework.MapRunner;
 import ch.zhaw.dna.ssh.mapreduce.model.framework.ReduceRunner;
 import ch.zhaw.dna.ssh.mapreduce.model.framework.ReduceTask;
 
 
 public class WordFrequencyReduceTask implements ReduceTask {
-	
-	public void reduce(ReduceRunner reduceRunner, MapRunner list) {
-		//Iterate over all entries with the same key and add the values
-		long value = 0;
+//	
+//	public void reduce(ReduceRunner reduceRunner, MapRunner list) {
+//		//Iterate over all entries with the same key and add the values
+//		long value = 0;
+//		
+//		for(String word : input) {
+//			value +=  Long.parseLong(word);
+//		}
+//		System.out.println(Long.toString(value));
+//		
+//		//Pool.registerReduce(mapRunner);
+//	}
+
+	@Override
+	public void reduce(ReduceRunner reduceRunner, List<MapRunner> mapRunners) {
+		// TODO Auto-generated method stub
 		
-		for(String word : input) {
-			value +=  Long.parseLong(word);
-		}
-		System.out.println(Long.toString(value));
-		
-		//Pool.registerReduce(mapRunner);
 	}
 	
 	
