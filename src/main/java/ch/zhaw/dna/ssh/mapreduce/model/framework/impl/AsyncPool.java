1a486a7b285dc1a99e817fbe1865776e0019ca83
Reto Habluetzel <rethab@rethab.ch>
2012-11-19 00:57:06 +0100

    use blocking queue instead of wait

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/impl/AsyncPool.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/impl/AsyncPool.java
@@ -1,18 +1,18 @@
 package ch.zhaw.dna.ssh.mapreduce.model.framework.impl;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.Executor;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import ch.zhaw.dna.ssh.mapreduce.model.framework.Pool;
 import ch.zhaw.dna.ssh.mapreduce.model.framework.Worker;
 import ch.zhaw.dna.ssh.mapreduce.model.framework.WorkerTask;
 
 /**
- * Ein Pool verwaltet Worker für Aufgaben die ausgeführt werden müssen. Dazu ist es möglich dem Pool Aufgaben und Worker zu übergeben.
+ * Ein Pool verwaltet Worker für Aufgaben die ausgeführt werden müssen. Dazu ist es möglich dem Pool Aufgaben und Worker
+ * zu übergeben.
  * 
  * @author Max, Desiree Sacher
  * 
@@ -20,30 +20,26 @@ import ch.zhaw.dna.ssh.mapreduce.model.framework.WorkerTask;
 public final class AsyncPool implements Runnable, Pool {
 
 	// Liste mit allen Workern
-	private final List<Worker> workingWorker;
+	private final Queue<Worker> workingWorker = new ConcurrentLinkedQueue<Worker>();
 
 	// Liste mit allen Workern, die Arbeit übernehmen können.
-	private final List<Worker> emptyWorkerList;
+	private final BlockingQueue<Worker> workerBlockingQueue = new LinkedBlockingQueue<Worker>();
 
 	// Liste mit aller Arbeit, die von Workern übernommen werden kann.
-	private final Queue<WorkerTask> toDoList;
+	private final Queue<WorkerTask> taskQueue = new ConcurrentLinkedQueue<WorkerTask>();
 
 	/**
-	 * Erstellt einen neuen Pool der gern Aufgaben und Worker entgegen nimmt.
+	 * Erstellt einen neuen Pool der Aufgaben und Worker entgegen nimmt.
 	 */
 	public AsyncPool() {
-		workingWorker = new ArrayList<Worker>();
-		emptyWorkerList = new ArrayList<Worker>();
-		toDoList = new LinkedList<WorkerTask>();
 	}
-	
+
 	/**
 	 * Startet den Thread zur asynchronen Arbeit
 	 */
 	@Override
 	public void init() {
-		Executor executor = Executors.newSingleThreadExecutor();
-		executor.execute(this);
+		Executors.newSingleThreadExecutor().execute(this);
 	}
 
 	/**
@@ -51,7 +47,7 @@ public final class AsyncPool implements Runnable, Pool {
 	 */
 	@Override
 	public int getCurrentPoolSize() {
-		return emptyWorkerList.size() + workingWorker.size();
+		return workerBlockingQueue.size() + workingWorker.size();
 	}
 
 	/**
@@ -59,7 +55,7 @@ public final class AsyncPool implements Runnable, Pool {
 	 */
 	@Override
 	public int getFreeWorkers() {
-		return emptyWorkerList.size();
+		return workerBlockingQueue.size();
 	}
 
 	/**
@@ -69,36 +65,27 @@ public final class AsyncPool implements Runnable, Pool {
 	public void run() {
 		try {
 			while (true) {
-				while (toDoList.size() > 0) {
-
-					if (emptyWorkerList.size() > 0) {
-						Worker nextWorker = emptyWorkerList.get(0);
-
-						// Die Task am Kopf der Queue asynchron ausführen
-						nextWorker.execute(toDoList.poll());
-						
-						emptyWorkerList.remove(nextWorker);
-						workingWorker.add(nextWorker);
-					} else {
-						// Es sind Aufgaben zu erfüllen! schnell schauen ob wieder freie worker vorhanden sind.
-						Thread.sleep(200);
-					}
+				WorkerTask task = this.taskQueue.poll();
+				if (task != null) {
+					Worker worker = this.workerBlockingQueue.take(); // blockiert, bis ein Worker frei ist
+					this.workingWorker.add(worker);
+					worker.execute(task);
+				} else {
+					Thread.sleep(100);
 				}
-				// Jede halbe Sekunde wird versucht Tasks abzuarbeiten
-				Thread.sleep(500);
 			}
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 		}
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void workerIsFinished(Worker finishedWorker){
+	public void workerIsFinished(Worker finishedWorker) {
 		workingWorker.remove(finishedWorker);
-		emptyWorkerList.add(finishedWorker);
+		workerBlockingQueue.add(finishedWorker);
 	}
 
 	/**
@@ -106,14 +93,14 @@ public final class AsyncPool implements Runnable, Pool {
 	 */
 	@Override
 	public void enqueueWork(WorkerTask task) {
-		toDoList.offer(task);
+		taskQueue.offer(task);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void donateWorker(Worker newWorker){
-		emptyWorkerList.add(newWorker);
+	public void donateWorker(Worker newWorker) {
+		workerBlockingQueue.add(newWorker);
 	}
 }
---
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/model/framework/AsyncPoolTest.java
@@ -0,0 +1,76 @@
+package ch.zhaw.dna.ssh.mapreduce.model.framework;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.concurrent.DeterministicExecutor;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.zhaw.dna.ssh.mapreduce.model.framework.impl.AsyncPool;
+import ch.zhaw.dna.ssh.mapreduce.model.framework.impl.ThreadWorker;
+
+@RunWith(JMock.class)
+public class AsyncPoolTest {
+	
+	private Mockery context;
+
+	@Before
+	public void initMock() {
+		this.context = new JUnit4Mockery();
+	}
+
+	@Test
+	public void shouldHaveZeroInitialWorker() {
+		AsyncPool p = new AsyncPool();
+		p.init();
+		assertEquals(0, p.getCurrentPoolSize());
+		assertEquals(0, p.getFreeWorkers());
+	}
+
+	@Test
+	public void shouldHaveOneWorker() {
+		Worker w = this.context.mock(Worker.class);
+		AsyncPool p = new AsyncPool();
+		p.init();
+		p.donateWorker(w);
+		assertEquals(1, p.getCurrentPoolSize());
+		assertEquals(1, p.getFreeWorkers());
+	}
+
+	@Test
+	public void shouldHaveTwoWorker() {
+		Worker w1 = this.context.mock(Worker.class, "w1");
+		Worker w2 = this.context.mock(Worker.class, "w2");
+		AsyncPool p = new AsyncPool();
+		p.init();
+		p.donateWorker(w1);
+		p.donateWorker(w2);
+		assertEquals(2, p.getCurrentPoolSize());
+		assertEquals(2, p.getFreeWorkers());
+	}
+
+	@Test
+	public void shouldExecuteWork() {
+		final WorkerTask task = this.context.mock(WorkerTask.class);
+		DeterministicExecutor exec = new DeterministicExecutor();
+		AsyncPool p = new AsyncPool();
+		p.init();
+		final ThreadWorker worker = new ThreadWorker(p, exec);
+		p.donateWorker(worker);
+
+		this.context.checking(new Expectations() {
+			{
+				one(task).doWork();
+			}
+		});
+
+		p.enqueueWork(task);
+		exec.runUntilIdle();
+	}
+
+}
