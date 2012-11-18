720a397622812162d615dedc9c4828e8ca71542a
Reto Habluetzel <rethab@rethab.ch>
2012-11-18 22:50:32 +0100

    make threadworkertest compile

---
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/model/framework/ThreadWorkerTest.java
@@ -0,0 +1,13 @@
+package ch.zhaw.dna.ssh.mapreduce.model.framework;
+
+import org.junit.Test;
+
+public class ThreadWorkerTest {
+	
+	@Test
+	public void test() {
+		Worker w;
+		//w.assignNextTask(doThis)
+	}
+
+}
