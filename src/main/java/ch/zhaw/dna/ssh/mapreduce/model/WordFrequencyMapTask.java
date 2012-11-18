ac05f5340f582a79a164ebeb50cf3704d81bf373
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-18 18:20:29 +0100

    created splitter for WordFrequencyMapTask

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/WordFrequencyMapTask.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/WordFrequencyMapTask.java
@@ -2,24 +2,48 @@ package ch.zhaw.dna.ssh.mapreduce.model;
 
 import ch.zhaw.dna.ssh.mapreduce.model.framework.MapRunner;
 import ch.zhaw.dna.ssh.mapreduce.model.framework.MapTask;
-
-public class WordFrequencyMapTask implements MapTask{
+/**
+ * Splittet jedes Wort und Emit'ed es an MapRunner
+ * @author des
+ *
+ */
+public class WordFrequencyMapTask implements MapTask {
 
 	@Override
 	public void map(MapRunner mapRunner, String[] todo) {
 		String[] text = todo;
 		int n = todo.length;
-		
+
 		for (int i = 0; i < n;) {
-			//Trim whitespaces
+
+			// Trim whitespaces
 			text[i] = text[i].trim();
-			System.out.println(text[i]);
+			if (text[i].contains(" ")) {
+				// white space index = wsi
+				int endIndex = text[i].indexOf(" ");
+				int beginIndex = 0;
+				int textLength;
+				
+				// Solange Whitespaces vorhanden, wird gesplittet
+				while (text[i].contains(" ")) {
+					textLength = text[i].length();
+					String splitted = text[i].substring(beginIndex, endIndex);
+					text[i] = text[i].substring(endIndex, textLength);
+					beginIndex = endIndex;
+					endIndex = text[i].length();
+					text[i] = text[i].trim();
+					mapRunner.emitIntermediateMapResult(splitted, "1");
+				}
+				// nach letztem Split noch letztes Wort hinzufuegen
+				mapRunner.emitIntermediateMapResult(text[i], "1");
+
+			} else {
+				// es hatte keine whitespaces, deswegen nur emiten
+				mapRunner.emitIntermediateMapResult(text[i], "1");
+			}
 			i++;
 		}
-		
-		
-		//Pool.registerMap(text[]);
-		
+
 	}
 
 }
