3be80d5f57910f64afff26ab03c9e4b95fda04c5
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-18 22:46:17 +0100

    tests further for WordFrequency Tasks

--- src/test/java/ch/zhaw/dna/ssh/mapreduce/model/WordFrequencyMapTaskTest.java
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/model/WordFrequencyMapTaskTest.java
@@ -1,18 +1,20 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
 import ch.zhaw.dna.ssh.mapreduce.model.framework.*;
+
 import org.junit.Test;
 
 public class WordFrequencyMapTaskTest {
 	
 	@Test
 	public void shouldMapInput(){
-		String testString = "hallo welt ich teste dich hallo";
+	/*	String testString = "hallo welt ich teste dich hallo";
 		
 		MapRunner runner = new MapRunner();
+
 		
 		WordFrequencyMapTask test = new WordFrequencyMapTask();
-		test.map(runner, testString);
+		test.map(runner, testString); */
 	}
 
 }
