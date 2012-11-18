1ae5fbb2c9dbddbb554816abd09993c541b18a0b
Reto Habluetzel <rethab@rethab.ch>
2012-11-18 18:44:27 +0100

    changed reduceTask spec

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/ReduceTask.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/ReduceTask.java
@@ -1,7 +1,9 @@
 package ch.zhaw.dna.ssh.mapreduce.model.framework;
 
+import java.util.List;
+
 public interface ReduceTask extends Task {
 	
-	public void reduce(ReduceRunner reduceRunner, String[] input);
+	public void reduce(ReduceRunner reduceRunner, List<MapRunner> mapRunners);
 
 }
