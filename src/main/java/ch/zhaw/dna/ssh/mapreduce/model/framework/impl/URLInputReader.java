01da08e30fa8eaa3a1885d50269d2d9f65a89cd5
Desiree Sacher <sachedes@students.zhaw.ch>
2012-12-02 22:47:48 +0100

    created/started URLInputReader

---
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/framework/impl/URLInputReader.java
@@ -0,0 +1,37 @@
+package ch.zhaw.dna.ssh.mapreduce.model.framework.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * URL einlesen: Mit Angabe der URL müssen ganze Webseiten eingelesen werden können
+ * Nach Tags soll automatisch gesucht und unterteilt werden können
+ * 
+ * @author des
+ *
+ */
+
+public class URLInputReader {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	public void readURL(String newinput) throws IOException{
+	
+	URL input = new URL(newinput);
+	BufferedReader in = new BufferedReader(
+			new InputStreamReader(input.openStream()));
+	
+	 String inputLine;
+     while ((inputLine = in.readLine()) != null)
+         System.out.println(inputLine);
+     in.close();
+	
+	}
+
+}
