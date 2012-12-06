f9e8627573068e47b6dec81843e820392e8d45c3
Desiree Sacher <sachedes@students.zhaw.ch>
2012-12-06 19:35:27 +0100

    updated test

--- src/test/java/ch/zhaw/dna/ssh/mapreduce/model/URLInputReaderTest.java
+++ src/test/java/ch/zhaw/dna/ssh/mapreduce/model/URLInputReaderTest.java
@@ -1,45 +1,26 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
- 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
-import org.jmock.integration.junit4.JUnit4Mockery;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import ch.zhaw.dna.ssh.mapreduce.model.URLInputReader;
-
-@RunWith(JMock.class)
 public class URLInputReaderTest {
-	
-	String newinput = "http://edu.panter.ch/MdpUebungDecorator";
-	
-	Mockery context;
-	
-	@Before
-	public void setUp() {
-		context = new JUnit4Mockery();
-	}
-	
-	
+
+	static String url = "http://edu.panter.ch/MdpUebungDecorator";
+
 	@Test
 	public void testInputURL() throws IOException {
-		
-		final URLInputReader testRun = context.mock(URLInputReader.class);
-		
-		context.checking(new Expectations() {
-			{
-			one(testRun).readURL(newinput);
-			}
-		});
-		URLInputReader test = new URLInputReader();
-		test.readURL("hallo welt ich teste dich hallo");
-		
+		URLInputReader uir = new URLInputReader();
+		assertNotNull(uir.readURL(url));
+
+	}
+
+	public void lookForTestWord() throws IOException {
+		URLInputReader uir = new URLInputReader();
+		assertTrue(uir.readURL(url).toString().contains("Pattern"));
 	}
 
 }
