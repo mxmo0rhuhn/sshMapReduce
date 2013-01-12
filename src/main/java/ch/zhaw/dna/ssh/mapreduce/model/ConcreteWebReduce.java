ba4d2099107f233fb6761e3258345e31df5eeb4a
Desiree Sacher <sachedes@students.zhaw.ch>
2013-01-12 14:17:50 +0100

    added reduce interface

---
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebReduce.java
@@ -0,0 +1,11 @@
+package ch.zhaw.dna.ssh.mapreduce.model;
+
+public interface ConcreteWebReduce {
+	
+	/**
+	 * Zählt zusammen was zusammen gehört
+	 * @return Number of Appearances von Maps
+	 */
+	public int getNumberOfOccurences();
+
+}
