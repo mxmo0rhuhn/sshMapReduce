3400fcd81d0141f6acb89ecc8dafcf7b864961e4
Desiree Sacher <sachedes@students.zhaw.ch>
2013-01-13 22:27:03 +0100

    corrected WebMap - 1. version

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebMap.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebMap.java
@@ -1,37 +1,74 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
-public interface ConcreteWebMap{
-	
+import ch.zhaw.mapreduce.MapEmitter;
+import ch.zhaw.mapreduce.MapInstruction;
 
-	/**
-	 * Durchsucht Webseite nach <p> Tags
-	 * Info: eventuell nicht nötig, bei nicht Bedarf entfernen
-	 * @param searchableText übergibt zu durchsuchenden Text
-	 * @return String mit gefundenen Tags
-	 */
-	public String getContentPTags(String searchableText);
-	/**
-	 * Druchsucht Webseite nach <q> Tags
-	 * Info: eventuell nicht nötig, bei nicht Bedarf entfernen
-	 * @param searchableText übergibt zu durchsuchenden Text
-	 * @return String mit gefundenen Tags
-	 */
-	public String getContentQTags(String searchableText);
+public class ConcreteWebMap implements MapInstruction {
 	
-	/**
-	 * Durchsucht Webseite nach Strukturmerkmalen und erkennt dadurch die Tiefe
-	 * hint: http://www.java2s.com/Code/Java/XML/TraversetheDOMtreeusingTreeWalker.htm
-	 * @param searchableText
-	 * @return int mit Anzahl Tiefen
-	 */
-	public int getContentDepth(String searchableText);
+	private boolean h1IsSet = false;
+	private boolean h2IsSet = false;
+	private boolean h3IsSet = false;
+	private boolean pIsSet = false;
+	private boolean aIsSet = false;
+
+
+	@Override
+	public void map(MapEmitter arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public boolean isH1IsSet() {
+		return h1IsSet;
+	}
+
+
+	public void setH1IsSet(boolean h1IsSet) {
+		this.h1IsSet = h1IsSet;
+	}
+
+
+	public boolean isH2IsSet() {
+		return h2IsSet;
+	}
+
+
+	public void setH2IsSet(boolean h2IsSet) {
+		this.h2IsSet = h2IsSet;
+	}
+
+
+	public boolean isH3IsSet() {
+		return h3IsSet;
+	}
+
+
+	public void setH3IsSet(boolean h3IsSet) {
+		this.h3IsSet = h3IsSet;
+	}
+
+
+	public boolean ispIsSet() {
+		return pIsSet;
+	}
+
+
+	public void setpIsSet(boolean pIsSet) {
+		this.pIsSet = pIsSet;
+	}
+
+
+	public boolean isaIsSet() {
+		return aIsSet;
+	}
+
+
+	public void setaIsSet(boolean aIsSet) {
+		this.aIsSet = aIsSet;
+	}
 	
-	/**
-	 * Durchsucht Webseite nach <a> Tags (Links auf andere Webseiten)
-	 * @return Link auf andere Seite
-	 * @return String mit gefundenen Tags
-	 */
-	public String getContentATags(String searchableText);
+	
 	
 
 }
