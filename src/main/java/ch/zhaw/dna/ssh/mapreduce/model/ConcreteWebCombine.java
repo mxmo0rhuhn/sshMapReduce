84470957edf10e3b7689e360ce1b8dc8a590fd23
Desiree Sacher <sachedes@students.zhaw.ch>
2013-01-13 22:33:59 +0100

    only changed interface part yet, still more work todo

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebCombine.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebCombine.java
@@ -1,12 +1,18 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
-public interface ConcreteWebCombine {
-	
-	/**
-	 * Methode um Informationen zu kombinieren, verhält sich unterschiedlich je nach Input (prüft auf was erhalten wurde)
-	 * @param combinableText
-	 * @return String mit kombiniertenInfos
-	 */
-	public String goCombineInformation(String combinableText);
+import java.util.Iterator;
+import java.util.List;
+
+import ch.zhaw.mapreduce.CombinerInstruction;
+import ch.zhaw.mapreduce.KeyValuePair;
+
+public class ConcreteWebCombine implements CombinerInstruction {
+
+
+	@Override
+	public List<KeyValuePair> combine(Iterator<KeyValuePair> toCombine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
