c9955062a38fbc8453d62e6497bd96a8c88f733e
mo0rhuhn <max-schrimpf@hotmail.com>
2013-01-15 19:25:02 +0100

    Performance Verbesserungen, verbesserung der Testbarkeit

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebCombine.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebCombine.java
@@ -1,18 +1,40 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import ch.zhaw.mapreduce.CombinerInstruction;
 import ch.zhaw.mapreduce.KeyValuePair;
 
 public class ConcreteWebCombine implements CombinerInstruction {
 
+	private final Map<String, StringBuilder> concatenatedStrings = new HashMap<String, StringBuilder>();
 
 	@Override
 	public List<KeyValuePair> combine(Iterator<KeyValuePair> toCombine) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
+		// TODO geht bessere Performance?
+
+		while (toCombine.hasNext()) {
+			KeyValuePair currentKeyValuePair = toCombine.next();
+			if (concatenatedStrings.containsKey(currentKeyValuePair.getKey())) {
+				concatenatedStrings.get(currentKeyValuePair.getKey()).append(
+						currentKeyValuePair.getValue());
+
+			} else {
+				concatenatedStrings.put(currentKeyValuePair.getKey(),
+						new StringBuilder(currentKeyValuePair.getValue()));
+			}
+
+		}
+		List<KeyValuePair> returnValues = new ArrayList<KeyValuePair>();
+		for (String key : concatenatedStrings.keySet()) {
+			returnValues.add(new KeyValuePair(key, concatenatedStrings.get(key)
+					.toString()));
+		}
+		return returnValues;
+	}
 }
