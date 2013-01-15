ef39ec5b5cd7db46dd28a7bf28a0bffa0b62fbd5
Reto Hablützel <rethab.ch@gmail.com>
2013-01-15 18:17:10 +0100

    implemented concreteWebMap

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebMap.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/ConcreteWebMap.java
@@ -1,74 +1,101 @@
 package ch.zhaw.dna.ssh.mapreduce.model;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import ch.zhaw.mapreduce.MapEmitter;
 import ch.zhaw.mapreduce.MapInstruction;
 
 public class ConcreteWebMap implements MapInstruction {
-	
-	private boolean h1IsSet = false;
-	private boolean h2IsSet = false;
-	private boolean h3IsSet = false;
-	private boolean pIsSet = false;
-	private boolean aIsSet = false;
 
+	private final Set<String> tags = new HashSet<String>(5);
+
+	private final DomParserFacade domParser = new DomParserFacade();
+	
+	private final URLInputReader reader;
+	
+	ConcreteWebMap(URLInputReader reader) {
+		this.reader = reader;
+	}
+	
+	public ConcreteWebMap() {
+		this(new URLInputReaderImpl());
+	}
 
 	@Override
-	public void map(MapEmitter arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
+	public void map(MapEmitter emitter, String url) {
+		String contents;
+		try {
+			contents = reader.readURL(url);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		// Der DOM-Parser liefert für den Inhalt einer Website (contents) und einem Array von Tags eine Map. Die Map hat
+		// als Keys sämtliche Tags, für die wir uns interessieren. Und als Values jeweils eine Liste mit den Texten zu
+		// den jeweiligen Tags. Es muss eine Liste sein, weil ein Tag auf einer Website mehrmals vorkommen kann.
+		// Sämtliche URLs, die auf einer Website gefunden werden, werden unter dem speziellen Key 'URLS' abgelegt.
+		Map<String, List<String>> tagsWithContent = domParser.extractText(contents,
+				tags.toArray(new String[tags.size()]));
+		for (Map.Entry<String, List<String>> entry : tagsWithContent.entrySet()) {
+			for (String text : entry.getValue()) {
+				emitter.emitIntermediateMapResult(entry.getKey(), text);
+			}
+		}
 	}
-
 
 	public boolean isH1IsSet() {
-		return h1IsSet;
+		return hasTag("h1");
 	}
-
 
 	public void setH1IsSet(boolean h1IsSet) {
-		this.h1IsSet = h1IsSet;
+		setTag("h1", h1IsSet);
 	}
-
 
 	public boolean isH2IsSet() {
-		return h2IsSet;
+		return hasTag("h2");
 	}
-
 
 	public void setH2IsSet(boolean h2IsSet) {
-		this.h2IsSet = h2IsSet;
+		setTag("h1", h2IsSet);
 	}
-
 
 	public boolean isH3IsSet() {
-		return h3IsSet;
+		return hasTag("h3");
 	}
-
 
 	public void setH3IsSet(boolean h3IsSet) {
-		this.h3IsSet = h3IsSet;
+		setTag("h3", h3IsSet);
 	}
-
 
 	public boolean ispIsSet() {
-		return pIsSet;
+		return hasTag("p");
 	}
-
 
 	public void setpIsSet(boolean pIsSet) {
-		this.pIsSet = pIsSet;
+		setTag("p", pIsSet);
 	}
-
 
 	public boolean isaIsSet() {
-		return aIsSet;
+		return hasTag("a");
 	}
-
 
 	public void setaIsSet(boolean aIsSet) {
-		this.aIsSet = aIsSet;
+		setTag("a", aIsSet);
 	}
-	
-	
-	
+
+	private boolean setTag(String tagName, boolean set) {
+		if (set) {
+			return this.tags.add(tagName);
+		} else {
+			return this.tags.remove(tagName);
+		}
+	}
+
+	private boolean hasTag(String tagName) {
+		return this.tags.contains(tagName);
+	}
 
 }
---
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/model/URLInputReaderImpl.java
@@ -0,0 +1,50 @@
+package ch.zhaw.dna.ssh.mapreduce.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * URL einlesen: Mit Angabe der URL müssen ganze Webseiten eingelesen werden
+ * können Nach Tags soll automatisch gesucht und unterteilt werden können DOM
+ * Struktur soll beachtet werden
+ * 
+ * Quelleninfos:
+ * http://www.java2s.com/Code/Java/XML/Searchupthetreeforagivennode.htm
+ * http://www.drdobbs.com/jvm/easy-dom-parsing-in-java/231002580
+ * 
+ * @author des
+ * 
+ */
+
+public class URLInputReaderImpl implements URLInputReader {
+
+	/**
+	 * Methode StringBuilder liest von URL alles als String ein
+	 * @param inputURL
+	 * 				URL welche ausgelesen werden soll
+	 * @return build
+	 * 				zusammengebauter Input als String
+	 * 
+	 */
+	@Override
+	public String readURL(String inputURL) throws IOException {
+
+		StringBuilder build = new StringBuilder();
+
+		URL input = new URL(inputURL);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				input.openStream()));
+		String inputLine;
+		try {
+			while ((inputLine = in.readLine()) != null) {
+				build.append(inputLine);
+			}
+		} finally {
+			in.close();
+		}
+		return build.toString();
+	}
+
+}
