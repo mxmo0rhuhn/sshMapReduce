f190ce36ac530eb266a83b0ff40350f6a6b13348
rethab <rethab@rethab.ch>
2013-04-22 22:44:20 +0200

    bindings for standalone

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/controller/ProjectLauncher.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/controller/ProjectLauncher.java
@@ -1,5 +1,6 @@
 package ch.zhaw.dna.ssh.mapreduce.controller;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import ch.zhaw.dna.ssh.mapreduce.model.WebCrawler;
@@ -8,7 +9,11 @@ import ch.zhaw.dna.ssh.mapreduce.view.util.SysoFrame;
 import ch.zhaw.mapreduce.Pool;
 import ch.zhaw.mapreduce.Worker;
 import ch.zhaw.mapreduce.plugins.thread.ThreadWorker;
-import ch.zhaw.mapreduce.registry.Registry;
+import ch.zhaw.mapreduce.registry.MapReduceConfig;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 /**
  * Diese Klasse startet die Applikation * @author Max
@@ -40,12 +45,20 @@ public class ProjectLauncher {
 	 * @param nworkers
 	 */
 	public void launch(int nworkers) {
+		Injector injector = Guice.createInjector(new MapReduceConfig(), new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(Worker.class).to(ThreadWorker.class);
+				bind(Executor.class).toInstance(Executors.newCachedThreadPool());
+			}
+		});
+
 		// TODO do not use registry directly
 		new SysoFrame();
 		System.out.println("Worker: " + nworkers);
-		Pool pool = Registry.getComponent(Pool.class);
-	for (int i = 1; i < nworkers; i++) {
-			pool.donateWorker(new ThreadWorker(pool, Executors.newSingleThreadExecutor(), new Provider<Persistence>()));
+		Pool pool = injector.getInstance(Pool.class);
+		for (int i = 1; i < nworkers; i++) {
+			pool.donateWorker(injector.getInstance(Worker.class));
 		}
 		OutputController out = new OutputController();
 		WebCrawler currentWebCrawler = new WebCrawler();
