71bd527cb6c14e5d6dba44efad80005ed0b61c96
rethab <rethab@rethab.ch>
2013-04-23 07:34:33 +0200

    enable thread plugin

--- src/main/java/ch/zhaw/dna/ssh/mapreduce/controller/ProjectLauncher.java
+++ src/main/java/ch/zhaw/dna/ssh/mapreduce/controller/ProjectLauncher.java
@@ -1,20 +1,9 @@
 package ch.zhaw.dna.ssh.mapreduce.controller;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
 import ch.zhaw.dna.ssh.mapreduce.model.WebCrawler;
 import ch.zhaw.dna.ssh.mapreduce.view.MainFrame;
-import ch.zhaw.dna.ssh.mapreduce.view.util.SysoFrame;
-import ch.zhaw.mapreduce.Pool;
 import ch.zhaw.mapreduce.ServerStarter;
-import ch.zhaw.mapreduce.Worker;
-import ch.zhaw.mapreduce.plugins.thread.ThreadWorker;
-import ch.zhaw.mapreduce.registry.MapReduceConfig;
-
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import ch.zhaw.mapreduce.plugins.thread.ThreadConfig;
 
 /**
  * Diese Klasse startet die Applikation * @author Max
@@ -29,9 +18,8 @@ public class ProjectLauncher {
 	 *            die nicht beachteten Übergabeparameter
 	 */
 	public static void main(String[] args) {
-		int nworker = args.length == 1 ? Integer.parseInt(args[0]) : Runtime.getRuntime().availableProcessors() + 1 ;
 		ProjectLauncher launcher = new ProjectLauncher();
-		launcher.launch(nworker);
+		launcher.launch(ThreadConfig.NWORKERS);
 	}
 
 	/**
@@ -46,10 +34,8 @@ public class ProjectLauncher {
 	 * @param nworkers
 	 */
 	public void launch(int nworkers) {
-		
+		System.setProperty("mrplugins", "Thread");
 		new ServerStarter().start();
-		// TODO do not use registry directly
-		new SysoFrame();
 		System.out.println("Worker: " + nworkers);
 
 		OutputController out = new OutputController();
